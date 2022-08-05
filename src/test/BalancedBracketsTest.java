70b6839ac4e450d777e636e38fb92c69fcb76d8d
Paulina Ponce <paulina.poncemarcano@gmail.com>
2022-08-04 20:33:05 -0400

    finished studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,58 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+     @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+     }
 
+    @Test
+    public void areBracketsCorrectOrder() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void noBracketsTest() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("hello"));
+    }
+
+    @Test
+    public void wordWithBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[hello]"));
+    }
+
+    @Test
+    public void wordWithOneBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[hello"));
+    }
+
+    @Test
+    public void wordAfterBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]hello"));
+    }
+
+    @Test
+    public void onlyOneBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void correctNestedBracketsTest() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Hello[there]]"));
+    }
+
+    @Test
+    public void incorrectNestedBracketsTest() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Hello[there]"));
+    }
+
+    @Test
+    public void notBalancedBracketsWord() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void wordWithBracketsInTheMiddle() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
 }
